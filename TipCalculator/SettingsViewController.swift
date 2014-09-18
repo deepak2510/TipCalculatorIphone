8d1a8da69779907fbe7068c5b11cbec324a00746
Deepak Bhagchandani <deepak.bhagchandani@pearson.com>
2014-09-17 23:21:59 -0700

    Tip Calculator

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Bhagchandani, Deepak on 9/17/14.
+//
+//
+
+import Foundation
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,15 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var billAmount : Double? = 0.0
+    var tipPercent : Double? = 15.0
+    var tipAmount : Double? = 0.0
+    var total : Double? = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.retrieveSetting()
+        self.tipPercentLabel.text = "Calculated Tip @\(self.tipSlider.value)%"
+   
     }
 
+    @IBAction func screenTapped(sender: AnyObject) {
+        view.endEditing(true);
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewWillAppear(animated: Bool) {
+        self.retrieveSetting()
+        
+    }
+    
+    func retrieveSetting(){
+        var defaults = NSUserDefaults.standardUserDefaults()
+    
+        if let x = defaults.objectForKey("tipPercent") as? Float {
+            var retrievedTip = x;
+            self.tipSlider.setValue(retrievedTip, animated: true)
+            self.tipPercent = retrievedTip._bridgeToObjectiveC().doubleValue;
+        }
+
+        self.recalculate()
+        
+
+    }
+    
+  
+    @IBAction func recalculate(){
+        
+       self.billAmount = self.billField.text.stringByTrimmingCharactersInSet(NSCharacterSet (charactersInString: "$"))._bridgeToObjectiveC().doubleValue
+    
+        self.tipAmount = self.billAmount! * (self.tipPercent!/100);
+        
+        self.total = self.billAmount! + self.tipAmount!;
+        
+        self.updateView();
+    
+    }
+    
+
+    
+    func updateView(){
+        if let tip = self.tipAmount {
+            self.tipLabel.text = "$\(self.tipAmount!)"
+        }
+        if let total = self.total {
+            self.totalLabel.text = "$\(total)"
+        
+        }
+        if let tipPercent = self.tipPercent {
+        self.tipPercentLabel.text = "Calculated Tip @\(tipPercent)%"
+        }
+        
+    }
+    
+    
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        var tipPercent = lroundf(sender.value)._bridgeToObjectiveC().doubleValue
+        self.tipPercent = tipPercent;
+        self.tipPercentLabel.text = "Calculated Tip @\(self.tipPercent!)%"
+        self.recalculate()
+    }
+
 
 
 }
